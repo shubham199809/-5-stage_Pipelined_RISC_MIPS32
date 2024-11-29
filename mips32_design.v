86e95e37b57d26f1f0c1f4b13695b4d2d225b393
shubham199809 <shubham.sml9@gmail.com>
2024-11-30 01:19:20 +0530

    mips32_design.v

--- mips32_design.v
+++ mips32_design.v
@@ -24,7 +24,7 @@
     if (HALTED == 0)
     begin
       if (((EX_MEM_IR[31:26] == BEQZ) && (EX_MEM_cond == 1)) ||
-          ((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 0)))
+          ((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 1)))
       begin
         IF_ID_IR <= #2 Mem[EX_MEM_ALUOut];
         TAKEN_BRANCH <= #2 1'b1;
